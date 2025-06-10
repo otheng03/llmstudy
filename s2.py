e7b4558fafd3649a3ec40efa92a12dd8242f7cbc
otheng03 <qnade@naver.com>
2025-06-11 05:14:12 +0900

    Fixed s2.py

--- s2.py
+++ s2.py
@@ -1,12 +1,13 @@
 from importlib.metadata import version
 import tiktoken
+
 print("tiktoken version:", version("tiktoken"))
 
 tokenizer = tiktoken.get_encoding("gpt2")
 
 text = (
     "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
-     "of someunknownPlace."
+    "of someunknownPlace."
 )
 integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
 print(integers)
@@ -14,3 +15,10 @@ print(integers)
 strings = tokenizer.decode(integers)
 print(strings)
 
+# Try the BPE tokenizer from the tiktoken library on the unknown words “Akwirw ier” and print the individual token IDs.
+# Then, call the decode function on each of the resulting integers in this list to reproduce the mapping.
+# Lastly, call the decode method on the token IDs to check whether it can reconstruct the original input, “Akwirw ier.”
+akwirwier = "Akwirw ier"
+akwirwier_integers = tokenizer.encode(akwirwier, allowed_special={"<|endoftext|>"})
+print({tokenizer.decode_single_token_bytes(i):i for i in akwirwier_integers})
+print(tokenizer.decode(akwirwier_integers))
