541cfdac93ae82a5ac94d432cd84be3a0c5779ca
hsh <hsh@localhost.localdomain>
2025-06-13 04:21:04 +0900

    Added s3.py

--- s2.py
+++ s2.py
@@ -22,3 +22,22 @@ akwirwier = "Akwirw ier"
 akwirwier_integers = tokenizer.encode(akwirwier, allowed_special={"<|endoftext|>"})
 print({tokenizer.decode_single_token_bytes(i):i for i in akwirwier_integers})
 print(tokenizer.decode(akwirwier_integers))
+
+with open("the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+
+enc_text = tokenizer.encode(raw_text)
+print(len(enc_text))
+
+enc_sample = enc_text[50:]
+
+context_size = 4
+x = enc_sample[:context_size]
+y = enc_sample[1:context_size+1]
+print(f"x: {x}")
+print(f"y:      {y}")
+
+for i in range(1, context_size+1):
+    context = enc_sample[:i]
+    desired = enc_sample[i]
+    print(tokenizer.decode(context), "---->", tokenizer.decode([desired]))
