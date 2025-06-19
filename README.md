75148bad69230e3bb7ed58ce31224c4147170a3c
otheng03 <07c00h@gmail.com>
2025-06-20 03:17:42 +0900

    Added zed env to README.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # llmstudy
+
+# Setup
+
+## Zed
+
+### Create pyproject.toml
+
+```
+$ $PROJECT_HOME/llmstudy pyproject.toml
+$ vi pyproject.toml
+```
+
+Type the content below, save pyproject.toml, quit vi
+```
+[tool.pyright]
+venvPath = "."
+venv = "venv"
+```
+
+Open zed
+```
+$ zed -n $PROJECT_HOME/llmstudy
+```
+
+### Install Ruff linter extension
+
+`cmd + shift + p` -> `zed: extensions` -> Install Ruff
+
+`cmd + shift + p` -> `zed: open project setting` -> Type the content below
+```
+{
+  "languages": {
+    "Python": {
+      "language_servers": ["ruff"]
+    }
+  }
+}
+```
--- s5.py
+++ s5.py
@@ -8,6 +8,7 @@ which are crucial components of the attention mechanism.
 As the model is exposed to more data during training, it adjusts these trainable weights.
 """
 
+
 class SelfAttention_v2(nn.Module):
     def __init__(self, d_in, d_out, dkv_bias=False):
         super().__init__()
@@ -23,17 +24,20 @@ class SelfAttention_v2(nn.Module):
         queries = self.W_quest(x)
         values = self.W_value(x)
         attn_scores = queries @ keys.T
-        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
         context_vec = attn_weights @ values
         return context_vec
 
+
 inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55]] # step     (x^6)
+    [
+        [0.43, 0.15, 0.89],  # Your     (x^1)
+        [0.55, 0.87, 0.66],  # journey  (x^2)
+        [0.57, 0.85, 0.64],  # starts   (x^3)
+        [0.22, 0.58, 0.33],  # with     (x^4)
+        [0.77, 0.25, 0.10],  # one      (x^5)
+        [0.05, 0.80, 0.55],
+    ]  # step     (x^6)
 )
 
 d_in = inputs.shape[1]
