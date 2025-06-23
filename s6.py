f023dd7bc7337a8127a246fae118fd4360c0d6c5
otheng03 <07c00h@gmail.com>
2025-06-24 00:22:24 +0900

    Minor fixes

--- s6.py
+++ s6.py
@@ -20,7 +20,7 @@ class DummyGPTModel(nn.Module):
         )
 
     def forward(self, in_idx):
-        batch_size, seq_len = in_idx.shape
+        batch_size, seqk_len = in_idx.shape
         tok_embeds = self.toks_emb(in_idx)
         pos_embeds = self.pos_emb(
             torch.arange(seq_len, device=in_idx.device),
@@ -69,11 +69,11 @@ GPT_CONFIG_124M = {
     "qkv_bias": False,  # Query-Key-Value bias
 }
 
-torch.manual_seed(123)
-model = DummyGPTModel(GPT_CONFIG_124M)
-logits = model(batch)
-print("Output shape:", logits.shape)
-print(logits)
+#torch.manual_seed(123)
+#model = DummyGPTModel(GPT_CONFIG_124M)
+#logits = model(batch)
+#print("Output shape:", logits.shape)
+#print(logits)
 
 torch.manual_seed(123)
 batch_example = torch.randn(2, 5)
@@ -81,3 +81,37 @@ layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
 out = layer(batch_example)
 print(out)
 
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs:\n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+torch.set_printoptions(sci_mode=False)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, keepdim=True, unbiased=False)
+print(f"[LayerNorm]\nMean: {mean}\nVariance: {var}\n")
