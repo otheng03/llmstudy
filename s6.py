5d7355a0084f7ee19a7fa00f0036b89844245786
otheng03 <07c00h@gmail.com>
2025-06-24 23:53:26 +0900

    Fixed s6.py

--- s6.py
+++ s6.py
@@ -176,3 +176,101 @@ block = TransformerBlock(GPT_CONFIG_124M)
 output = block(x)
 print("Input shape:", x.shape)
 print("Output shape:", output.shape)
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        """
+        It initializes the token and positional embeddings layers
+        using the configurations passed in via a Python dictionary, cfg.
+        These embedding layers are responsible for converting input
+        token indices into dense vectors and adding positional information.
+        :param cfg:
+        """
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+out = model(batch)
+print("Input batch:\n", batch)
+print("\nOutput shape:", out.shape)
+print(out)
+
+total_params = sum(p.numel() for p in model.parameters())
+print(f"Total number of parameters: {total_params:,}")
+
+print("Token embedding layer shape:", model.tok_emb.weight.shape)
+print("Output layer shape:", model.out_head.weight.shape)
+
+total_params_gpt2 = (
+    total_params - sum(p.numel()
+    for p in model.out_head.parameters())
+)
+print(f"Number of trainable parameters "
+      f"considering weight tying: {total_params_gpt2:,}"
+)
+
+total_size_bytes = total_params * 4
+total_size_mb = total_size_bytes / (1024 * 1024)
+print(f"Total size of the model: {total_size_mb:.2f} MB")
+
+def generate_text_simple(model, idx,
+                         max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+print("encoded:", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("encoded_tensor.shape:", encoded_tensor.shape)
+
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"]
+)
+print("Output:", out)
+print("Output length:", len(out[0]))
+
+# The model generates gibberish, which is not at all like the coherent text
+# "Hello, I am a model ready to help".
+# The reason the model is unable to produce coherent text is that we haven't
+# trained it yet. So far, we have only implemented the GPT architecture and
+# initialized a GPT model instance with initial random weights.
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print(decoded_text)
