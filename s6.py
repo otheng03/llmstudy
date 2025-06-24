aba56d6eaaf11f62fdffdfdaaca91163ca1c71ce
otheng03 <07c00h@gmail.com>
2025-06-24 22:45:08 +0900

    Added attention.py and TransformerBlock

--- s6.py
+++ s6.py
@@ -1,29 +1,29 @@
 import tiktoken
 import torch
 import torch.nn as nn
+import attention
 
 
 class DummyGPTModel(nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.toks_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
         self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
         self.drop_emb = nn.Dropout(cfg["drop_rate"])
         self.trf_blocks = nn.Sequential(
-            *[DummyTransformerBlock(cfg) for _ in range(cfg["n_layers"])],
+            *[DummyTransformerBlock(cfg)
+              for _ in range(cfg["n_layers"])]
         )
         self.final_norm = DummyLayerNorm(cfg["emb_dim"])
         self.out_head = nn.Linear(
-            cfg["emb_dim"],
-            cfg["vocab_size"],
-            bias=False,
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
         )
 
     def forward(self, in_idx):
-        batch_size, seqk_len = in_idx.shape
-        tok_embeds = self.toks_emb(in_idx)
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
         pos_embeds = self.pos_emb(
-            torch.arange(seq_len, device=in_idx.device),
+            torch.arange(seq_len, device=in_idx.device)
         )
         x = tok_embeds + pos_embeds
         x = self.drop_emb(x)
@@ -32,7 +32,6 @@ class DummyGPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
 
-
 class DummyTransformerBlock(nn.Module):
     def __init__(self, cfg):
         super().__init__()
@@ -40,14 +39,12 @@ class DummyTransformerBlock(nn.Module):
     def forward(self, x):
         return x
 
-
 class DummyLayerNorm(nn.Module):
     def __init__(self, normalized_shape, eps=1e-5):
         super().__init__()
 
     def forward(self, x):
         return x
-
 
 tokenizer = tiktoken.get_encoding("gpt2")
 batch = []
@@ -69,11 +66,11 @@ GPT_CONFIG_124M = {
     "qkv_bias": False,  # Query-Key-Value bias
 }
 
-#torch.manual_seed(123)
-#model = DummyGPTModel(GPT_CONFIG_124M)
-#logits = model(batch)
-#print("Output shape:", logits.shape)
-#print(logits)
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
 
 torch.manual_seed(123)
 batch_example = torch.randn(2, 5)
@@ -115,3 +112,67 @@ out_ln = ln(batch_example)
 mean = out_ln.mean(dim=-1, keepdim=True)
 var = out_ln.var(dim=-1, keepdim=True, unbiased=False)
 print(f"[LayerNorm]\nMean: {mean}\nVariance: {var}\n")
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+    
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(out.shape)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = attention.MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+torch.manual_seed(123)
+x = torch.rand(2, 4, 768)
+block = TransformerBlock(GPT_CONFIG_124M)
+output = block(x)
+print("Input shape:", x.shape)
+print("Output shape:", output.shape)
